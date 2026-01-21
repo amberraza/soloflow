d198a8a6676d1d9135875831b28169a1ad6394aa
Amber Raza <amberraza.cs@gmail.com>
2026-01-21 02:41:59 -0500

    Fix: Clear wizard store on submit and fix reset logic

--- frontend/src/store/useFinancialStore.js
+++ frontend/src/store/useFinancialStore.js
@@ -1,44 +1,46 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+const INITIAL_STATE = {
+    caseBasics: {
+        plaintiff: '',
+        defendant: '',
+        county: '',
+        caseNumber: ''
+    },
+    income: {
+        grossMonthly: 0,
+        frequency: 'MONTHLY'
+    },
+    deductions: {
+        federalTax: 0,
+        stateTax: 0,
+        fica: 0,
+        healthInsurance: 0
+    },
+    expenses: {
+        housing: 0,
+        utilities: 0,
+        food: 0
+    },
+    childSupport: {
+        numChildren: 1,
+        motherOvernights: 182,
+        fatherOvernights: 183
+    }
+};
+
 const useFinancialStore = create(
     persist(
         (set) => ({
-            wizardData: {
-                caseBasics: {
-                    plaintiff: '',
-                    defendant: '',
-                    county: '',
-                    caseNumber: ''
-                },
-                income: {
-                    grossMonthly: 0,
-                    frequency: 'MONTHLY'
-                },
-                deductions: {
-                    federalTax: 0,
-                    stateTax: 0,
-                    fica: 0,
-                    healthInsurance: 0
-                },
-                expenses: {
-                    housing: 0,
-                    utilities: 0,
-                    food: 0
-                },
-                childSupport: {
-                    numChildren: 1,
-                    motherOvernights: 182,
-                    fatherOvernights: 183
-                }
-            },
+            wizardData: INITIAL_STATE,
             updateSection: (section, data) => set((state) => ({
                 wizardData: {
                     ...state.wizardData,
                     [section]: { ...state.wizardData[section], ...data }
                 }
             })),
-            reset: () => set({ wizardData: {} }) // Should reset to initial structure
+            reset: () => set({ wizardData: INITIAL_STATE })
         }),
         {
             name: 'soloflow-wizard-storage', // name of the item in the storage (must be unique)
